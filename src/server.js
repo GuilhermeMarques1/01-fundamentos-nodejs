2399e63f91d26ea473e7b6972b87e8433fa06afb
GuilhermeMarques1 <g-marques@outlook.com>
2024-04-19 00:00:16 -0300

    Init node api

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import http from "node:http";
+
+const users = [];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .writeHead(200)
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "john.doe@example.com",
+    });
+
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end();
+});
+
+server.listen(3333);
