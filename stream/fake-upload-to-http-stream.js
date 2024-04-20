61803c0e78dc3acd8e329d584d5d1f7f5c37760f
GuilhermeMarques1 <g-marques@outlook.com>
2024-04-20 12:06:07 -0300

    processing full stream

--- stream/fake-upload-to-http-stream.js
+++ stream/fake-upload-to-http-stream.js
@@ -22,4 +22,10 @@ fetch("http://localhost:3334", {
   method: "POST",
   body: new oneToHundredStream(),
   duplex: "half",
-});
+})
+  .then((res) => {
+    return res.text();
+  })
+  .then((data) => {
+    console.log(data);
+  });
