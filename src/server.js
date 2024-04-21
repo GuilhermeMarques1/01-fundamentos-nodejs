ef5ecde7755d2e686a5383469d84f78ea5db6e81
GuilhermeMarques1 <g-marques@outlook.com>
2024-04-21 12:02:55 -0300

    create database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
+import { Database } from "./database.js";
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -9,17 +10,21 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
+
     return res.writeHead(200).end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
     const { id, nome, email } = req.body;
 
-    users.push({
+    const user = {
       id,
       nome,
       email,
-    });
+    };
+
+    database.insert("users", user);
 
     return res.writeHead(201).end();
   }
