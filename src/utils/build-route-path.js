c756f2d782a734b0e61f30daf85179d307849f3e
GuilhermeMarques1 <g-marques@outlook.com>
2024-04-27 20:51:29 -0300

    params in routes

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,14 @@
 export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-Z]+)/g;
 
-  console.log(Array.from(path.matchAll(routeParametersRegex)));
+  // console.log(Array.from(path.matchAll(routeParametersRegex)));
+
+  // prettier-ignore
+  const pathWithParams = path.replaceAll(routeParametersRegex, "(?<$1>[a-z0-9\-_]+)");
+
+  // console.log(pathWithParams);
+
+  const pathRegex = new RegExp(`^${pathWithParams}`);
+
+  return pathRegex;
 }
