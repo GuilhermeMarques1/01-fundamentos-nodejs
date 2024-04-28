5f959346ccc10c3ee1f02007162093bf74358bb3
GuilhermeMarques1 <g-marques@outlook.com>
2024-04-28 11:52:47 -0300

    remove not used variable

--- src/server.js
+++ src/server.js
@@ -14,8 +14,6 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path);
 
-    const params = routeParams.groups;
-
     req.params = { ...routeParams.groups };
 
     // console.log(req.params);
