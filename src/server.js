e4dc656b1db8799d9b18769f64a1033e4982272b
GuilhermeMarques1 <g-marques@outlook.com>
2024-04-28 12:34:52 -0300

    Get query parameters

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -14,9 +15,18 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path);
 
-    req.params = { ...routeParams.groups };
+    // console.log(routeParams.groups);
 
-    // console.log(req.params);
+    const { query, ...params } = routeParams.groups;
+    // console.log(params);
+    // console.log(query);
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
+
+    // console.log("groups: ", req.params);
+    // console.log("----");
+    // console.log(extractQueryParams(req.params.query));
 
     return route.handler(req, res);
   }
